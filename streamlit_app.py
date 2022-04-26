f122d9d6a953e2a5a841be0903194d29bb43b152
Sasi_official <42794655+Sasi-007@users.noreply.github.com>
2022-04-26 13:39:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
 streamlit.title('My Mom\'s new healthy diner')
-streamlit.header('Breakfast menu')
-streamlit.text('Omega 2 and blueberry')
-streamlit.text('kale')
-streamlit.text('Hard-bottled')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
